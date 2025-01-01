8c57af4891b29891d0d1d339b2bc1ca949daa27d
leeshin20 <leeshin20@naver.com>
2025-01-01 20:03:49 +0900

    feat : Anime, Record, Series 도메인 작성

---
+++ src/main/java/backend/minori/domain/Anime.java
@@ -0,0 +1,51 @@
+package backend.minori.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "anime")
+public class Anime extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long animeId;
+
+    @Column(nullable = false, length = 100)
+    private String titleKr;
+
+    @Column(length = 100)
+    private String titleEn;
+
+    @Column(length = 100)
+    private String titleJp;
+
+    @Column(nullable = false)
+    private String airingStatus;
+
+    @Column(length = 10)
+    private String airingDay;
+
+    private Integer totalEpisodeNumber;
+
+    @Column(length = 30)
+    private String production;
+
+    @Column(length = 20)
+    private String platform;
+
+    @Column(length = 500)
+    private String storyDescription;
+
+    @Column(length = 20)
+    private String director;
+
+    @Column(length = 10)
+    private String airingType;
+
+    @ManyToOne
+    @JoinColumn(name = "seried_id")
+    private Series series;
+}
