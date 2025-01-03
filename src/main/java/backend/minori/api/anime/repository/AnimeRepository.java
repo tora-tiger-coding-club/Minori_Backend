9a7fec608ab975402931809e7f9e1ea1038f80a6
leeshin20 <leeshin20@naver.com>
2025-01-03 20:31:08 +0900

    feat : 전체 애니메이션 조회 api 구현

---
+++ src/main/java/backend/minori/api/anime/repository/AnimeRepository.java
@@ -0,0 +1,9 @@
+package backend.minori.api.anime.repository;
+
+import backend.minori.domain.Anime;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AnimeRepository extends JpaRepository<Anime, Long> {
+}
